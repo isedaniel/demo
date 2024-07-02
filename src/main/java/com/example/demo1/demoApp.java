4281aa125d208fadbf85abd6564abf63c78e1762
Daniel <ise.daniel@gmail.com>
2024-07-02 14:01:45 -0300

    stage color, stage size (Width x Height), stage Icons add, stage title

--- src/main/java/com/example/demo1/demoApp.java
+++ src/main/java/com/example/demo1/demoApp.java
@@ -2,6 +2,8 @@ package com.example.demo1;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 
@@ -11,8 +13,14 @@ public class demoApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Group root = new Group();
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, Color.BLACK);
 
+        Image icon = new Image("icon.png");
+
+        stage.setWidth(800);
+        stage.setHeight(600);
+        stage.getIcons().add(icon);
+        stage.setTitle("Demo App");
         stage.setScene(scene);
         stage.show();
     }
