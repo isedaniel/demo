69374b2ef431c1c5c1ad4db5a2c7005d1a090089
Daniel <ise.daniel@gmail.com>
2024-07-08 12:02:31 -0300

    sending information between screens

---
+++ src/main/java/com/example/demo/scene1Controller.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class scene1Controller {
+
+    public void login(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("scene2.fxml")));
+        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
--- src/main/java/com/example/demo/scene2Controller.java
+++ src/main/java/com/example/demo/scene2Controller.java
@@ -10,21 +10,13 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
-public class sceneController {
+public class scene2Controller {
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-    public void switchScene1(ActionEvent e) throws IOException {
+    public void logout(ActionEvent e) throws IOException {
         root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("scene1.fxml")));
-        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public void switchScene2(ActionEvent e) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("scene2.fxml")));
         stage = (Stage)((Node)e.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
