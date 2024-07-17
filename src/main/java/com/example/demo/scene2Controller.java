d572dbfd27dde589f6075d252850dd89625586ae
Daniel <ise.daniel@gmail.com>
2024-07-17 14:06:00 -0300

    closing but memory leak not solved yet

--- src/main/java/com/example/demo/scene2Controller.java
+++ src/main/java/com/example/demo/scene2Controller.java
@@ -22,10 +22,7 @@ public class scene2Controller {
     }
 
     public void logout(ActionEvent e) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("scene1.fxml")));
         Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        stage.close();
     }
 }
