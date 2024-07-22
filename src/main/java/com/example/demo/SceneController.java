dae2b224098b377488dfef2315ae8c0915aee648
Daniel <ise.daniel@gmail.com>
2024-07-22 12:30:40 -0300

    write it again from scratch to practice

--- src/main/java/com/example/demo/SceneController.java
+++ src/main/java/com/example/demo/SceneController.java
@@ -6,21 +6,21 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class SceneController {
-    @FXML
-    ImageView imgView;
-
-    int imageNum = 1;
-
+    int imageNumber = 1;
     Image image1 = new Image(getClass().getResourceAsStream("/img1.png"));
     Image image2 = new Image(getClass().getResourceAsStream("/img2.png"));
+    @FXML
+    ImageView imageView;
 
-    public void changeImg(ActionEvent e) {
-        if (imageNum == 1) {
-            imgView.setImage(image2);
-            imageNum = 2;
-        } else {
-            imgView.setImage(image1);
-            imageNum = 1;
+
+    public void changeImage(ActionEvent e) {
+        if (imageNumber == 1) {
+            imageNumber = 2;
+            imageView.setImage(image2);
+        }
+        else {
+            imageNumber = 1;
+            imageView.setImage(image1);
         }
     }
 }
--- src/main/java/com/example/demo/demoApp.java
+++ src/main/java/com/example/demo/demoApp.java
@@ -7,17 +7,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class demoApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Scene.fxml"));
+
         Scene scene = new Scene(root);
 
         stage.setScene(scene);
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch(args);
