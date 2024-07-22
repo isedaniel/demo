68ee4a9a70e64ce41df6acb0821f7028d1a47c4b
Daniel <ise.daniel@gmail.com>
2024-07-22 11:10:17 -0300

    made it work back and forth

--- src/main/java/com/example/demo/SceneController.java
+++ src/main/java/com/example/demo/SceneController.java
@@ -2,14 +2,25 @@ package com.example.demo;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class SceneController {
     @FXML
-    ImageView imgViewer;
+    ImageView imgView;
+
+    int imageNum = 1;
+
+    Image image1 = new Image(getClass().getResourceAsStream("/img1.png"));
+    Image image2 = new Image(getClass().getResourceAsStream("/img2.png"));
 
     public void changeImg(ActionEvent e) {
-        imgViewer.setImage("./img2.png");
+        if (imageNum == 1) {
+            imgView.setImage(image2);
+            imageNum = 2;
+        } else {
+            imgView.setImage(image1);
+            imageNum = 1;
+        }
     }
 }
